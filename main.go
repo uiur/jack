f1aa1b66c9c14feceebc05660d9878f3aca82655
Kazato Sugimoto <uiureo@gmail.com>
2016-02-15 16:17:18 +0900

    Implement buildSymbolTable

--- main.go
+++ main.go
@@ -27,3 +27,120 @@ func main() {
 
 	fmt.Print(tree.ToXML())
 }
+
+// class Square {
+// 	field int x, y;
+// 	field int size;
+//
+// 	constructor Square new(int Ax, int Ay) {
+// 		var boolean a;
+//
+// 		let x = Ax;
+// 		let y = Ay;
+//
+// 		return this;
+// 	 }
+// }
+// SymbolTable
+// table.look("a") //=> {"local", "boolean", 0}
+// [
+// 	{Ax: {"argument", "int", 0}, Ay: {"argument", "int", 1}, a: {"local", "boolean", 2} },
+// 	{x: {"field", "int", 0}, y: {"field", "int", 1}, size: {"field", "int", 2}},
+// ]
+
+type Symbol struct {
+	// Kind: var, argument, static, field, class, subroutine
+	SymbolType, Kind string
+	Number           int
+}
+
+type SymbolTable struct {
+	Scopes []map[string]*Symbol
+}
+
+// compile(node *Node) string
+// compileClass(node *Node, table *SymbolTable) string
+// compileSubroutineDec(node *Node, table *SymbolTable) string
+
+func buildSymbolTable(node *parser.Node, base *SymbolTable) *SymbolTable {
+	if base == nil {
+		base = &SymbolTable{}
+	}
+
+	table := &SymbolTable{}
+
+	currentScope := map[string]*Symbol{}
+
+	baseScopes := []map[string]*Symbol{}
+	copy(baseScopes, base.Scopes)
+
+	table.Scopes = append([]map[string]*Symbol{currentScope}, baseScopes...)
+
+	switch node.Name {
+	case "class":
+		for _, node := range node.Children {
+			if node.Name == "classVarDec" {
+				kind := node.Children[0].Value
+				symbolType := node.Children[1].Value
+
+				names := []string{}
+				for _, node := range node.Children[2:] {
+					if node.Name == "identifier" {
+						names = append(names, node.Value)
+					}
+				}
+
+				for _, name := range names {
+					currentScope[name] = &Symbol{SymbolType: symbolType, Kind: kind, Number: len(currentScope)}
+				}
+			}
+		}
+	case "subroutineDec":
+		var parameterList *parser.Node
+
+		for _, node := range node.Children {
+			if node.Name == "parameterList" {
+				parameterList = node
+				break
+			}
+		}
+
+		for i, node := range parameterList.Children {
+			if node.Name == "keyword" {
+				keyword := node
+				identifier := parameterList.Children[i+1]
+				name := identifier.Value
+
+				currentScope[name] = &Symbol{
+					SymbolType: keyword.Value,
+					Kind:       "argument",
+					Number:     len(currentScope),
+				}
+			}
+		}
+
+		subroutineBody, _ := node.Find(&parser.Node{Name: "subroutineBody"})
+		for _, node := range subroutineBody.Children {
+			if node.Name == "varDec" {
+				symbolType := node.Children[1].Value
+
+				names := []string{}
+				for _, node := range node.Children {
+					if node.Name == "identifier" {
+						names = append(names, node.Value)
+					}
+				}
+
+				for _, name := range names {
+					currentScope[name] = &Symbol{
+						SymbolType: symbolType,
+						Kind:       "var",
+						Number:     len(currentScope),
+					}
+				}
+			}
+		}
+	}
+
+	return table
+}
--- main_test.go
+++ main_test.go
@@ -47,3 +47,80 @@ func testMainOutput(t *testing.T, jackFile string) {
 		t.Errorf("%s: %s %v", name, output, err)
 	}
 }
+
+func TestBuildSymbolTableFromClass(t *testing.T) {
+	node := parser.Parse(tokenizer.Tokenize(`
+    class Square {
+    	field int x, y;
+    	static String s;
+
+    	constructor Square new(int Ax, int Ay) {
+    		var boolean a;
+
+    		let x = Ax;
+    		let y = Ay;
+
+    		return this;
+    	}
+    }
+  `))
+
+	table := buildSymbolTable(node, nil)
+	if len(table.Scopes) != 1 {
+		t.Errorf("expect table to have 1 scopes, actual: %d", len(table.Scopes))
+		return
+	}
+
+	expectedScope := map[string]*Symbol{
+		"x": {"int", "field", 0},
+		"y": {"int", "field", 1},
+		"s": {"String", "static", 2},
+	}
+
+	scope := table.Scopes[0]
+
+	testScopeMatch(t, scope, expectedScope)
+}
+
+func TestBuildSymbolTableFromSubroutine(t *testing.T) {
+	node := parser.Parse(tokenizer.Tokenize(`
+    class Square {
+    	field int x, y;
+    	static String s;
+
+    	constructor Square new(int Ax, int Ay) {
+    		var boolean a, b;
+
+    		let x = Ax;
+    		let y = Ay;
+
+    		return this;
+    	}
+    }
+  `))
+
+	subroutineDec, _ := node.Find(&parser.Node{Name: "subroutineDec"})
+
+	table := buildSymbolTable(subroutineDec, nil)
+
+	testScopeMatch(t, table.Scopes[0], map[string]*Symbol{
+		"Ax": {"int", "argument", 0},
+		"Ay": {"int", "argument", 1},
+		"a":  {"boolean", "var", 2},
+		"b":  {"boolean", "var", 3},
+	})
+}
+
+func testScopeMatch(t *testing.T, scope, expectedScope map[string]*Symbol) {
+	if len(scope) != len(expectedScope) {
+		t.Errorf("scope should have %v symbols, but actual: %v", len(expectedScope), len(scope))
+		return
+	}
+
+	for key, expectedSymbol := range expectedScope {
+		symbol := scope[key]
+		if !(symbol != nil && symbol.SymbolType == expectedSymbol.SymbolType && symbol.Kind == expectedSymbol.Kind && symbol.Number == expectedSymbol.Number) {
+			t.Errorf("expect: %v, actual: %v", expectedScope, scope)
+		}
+	}
+}
